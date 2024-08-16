297ebe0be9ef8806e58ea50e3f43dd2d213e5904
c1kvoy <seva33039@gmail.com>
2024-08-16 18:05:32 +0700

    Plane struct added, side_check added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+
+
 struct Point
 {
     double x;
@@ -10,12 +12,38 @@ struct Point
 
     Point(double x, double y, double z): x(x), y(y), z(z) {}
 
-    Point vector_by_two_points(Point& other) const {
+    Point vector_by_two_points(const Point& other) const {
         return Point(x - other.x, y - other.y, z - other.z);
     }
 
-    double length_of_vector() const {
-        return std::sqrt(std::pow(x, 2) + std::pow(y, 2) + std::pow(z, 2));
+    double length() const {
+        return sqrt(x * x + y * y + z * z);
+    }
+
+    Point vector_multiply(const Point& other) const {
+        return Point(y * other.z - z * other.y,
+                     z * other.x - x * other.z,
+                     x * other.y - y * other.x);
+    }
+};
+
+
+struct Plane {
+    double A, B, C, D;
+
+    Plane(const Point& p1, const Point& p2, const Point& p3) {
+        Point v1 = p2.vector_by_two_points(p1);
+        Point v2 = p3.vector_by_two_points(p1);
+        Point normal = v1.vector_multiply(v2);
+
+        A = normal.x;
+        B = normal.y;
+        C = normal.z;
+        D = -(A * p1.x + B * p1.y + C * p1.z);
+    }
+
+    double side_check(const Point& p) const {
+        return A * p.x + B * p.y + C * p.z + D;
     }
 };
 
@@ -25,14 +53,11 @@ struct Triangle {
     Point p2;
     Point p3;
 
-    double triangle_area() {
+    double triangle_area() const {
         Point v1 = p2.vector_by_two_points(p1);
         Point v2 = p3.vector_by_two_points(p1);
 
-        Point vect_mul(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
-
-        cout << vect_mul.length_of_vector()/2 << endl;
-
+        return v1.vector_multiply(v2).length()/2;
     }
 
     Triangle(const Point& p1, const Point& p2, const Point& p3): p1(p1), p2(p2), p3(p3) {}
@@ -44,6 +69,7 @@ int main() {
     Point point3(3,4,0);
 
     Triangle triangle(point1, point2, point3);
-    triangle.triangle_area();
+    double area = triangle.triangle_area();
+    cout << area;
     return 0;
 }
